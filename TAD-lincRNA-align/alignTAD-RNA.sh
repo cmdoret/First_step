4a5ecfa6ef08c47b2ee58de7078220d536de7df4
cmdoret <cyril.mattheydoret@gmail.com>
2016-10-04 09:45:12 +0200

    implemented bsub in the script to avoid any risk of running the command on front end server

--- TAD-lincRNA-align/alignTAD-RNA.sh
+++ TAD-lincRNA-align/alignTAD-RNA.sh
@@ -3,5 +3,5 @@
 #This script intends to align lincRNA with TAD boundaries. It outputs RNAs which have at least
 #25% of their sequence overlapping a TAD boundary.
 
-bedtools intersect -a ../data/LCL.expressed.lincRNA.bed -b GM12878_TAD_boundaries.bed -f 0.25 -wa > lincRNA_25overlap_TADb.bed
+bsub -q priority 'bedtools intersect -a ../data/LCL.expressed.lincRNA.bed -b GM12878_TAD_boundaries.bed -f 0.25 -wa > lincRNA_25overlap_TADb.bed'
 
