f8fa4201f93a98f072c77b98d807e6181c3e7161
cmdoret <cyril.mattheydoret@gmail.com>
2016-10-05 16:42:41 +0200

    Ordered the script by separating pcgenes and lincRNA sections.

--- TAD-lincRNA-overlap/overlapTAD-RNA.R
+++ TAD-lincRNA-overlap/overlapTAD-RNA.R
@@ -27,43 +27,74 @@ TAD_boundaries <- rbind(TAD_boundaries_start,TAD_boundaries_end)
 # generating new file with all TAD boundaries.
 write.table(TAD_boundaries,file = "TAD_boundaries.bed",sep="\t",quote = F,col.names = F,row.names = F)
 
-# Next step: align lincRNAs to the TAD boundaries. Retain RNAs with at least 25% of their sequence aligned to a TAD boundary.
-# calculating length of all RNAs
-len_RNA <- cbind(RNA_ori,length=RNA_ori[,3]-RNA_ori[,2])
+# Next step: overlap lincRNAs to the TAD boundaries. Retain RNAs with at least 25% of their sequence aligned to a TAD boundary.
 
 # see bash script named "alignTAD-RNA.sh" using bedtools intersect.
 
 #====================================================
+#lincRNAs:
 
-#Alignment performed, contains duplicates: Some (many) transcripts seem to overlap more than 1 TAD boundary.
+#overlap performed, contains duplicates: Some (many) transcripts seem to overlap more than 1 TAD boundary.
 overlap_RNA <- read.table("lincRNA_25overlap_TADb.bed")
 colnames(overlap_RNA) <- c("chr", "start", "end", "gene", "strand")
+
+
 length(overlap_RNA$gene)
-# 2554 overlap events were found by the intersect program with -f 0.25
+# lincRNA: 2554 overlap events were found by the intersect program with -f 0.25
+
 length(overlap_RNA$gene[duplicated(overlap_RNA$gene)])
-# 1134 are duplicates
-#Almost half of the transcript overlap more than 1 TAD boundary. Is this possible ?
+# lincRNA: 1134 are duplicates
+
 #Anyway, removing duplicates
 TADbound_lincRNA <- overlap_RNA[!duplicated(overlap_RNA$gene),]
-# This means: Out of the 2510 original transcripts, 1420 have at least 25% of their sequence overlapping the TAD boundaries.
+# lincRNA: Out of the 2510 original transcripts, 1420 have at least 25% of their sequence overlapping the TAD boundaries.
 write.table(TADbound_lincRNA,file = "TADbound-lincRNA.bed",sep="\t",quote = F,col.names = F,row.names = F)
 # Those are the TADbound-lincRNAs
 
 #Identifying nonTADbound-lincRNAs
 nonTADbound_lincRNA <- RNA_ori[!(RNA_ori$gene %in% TADbound_lincRNA$gene),]
 write.table(nonTADbound_lincRNA,file = "nonTADbound-lincRNA.bed",sep="\t",quote = F,col.names = F,row.names = F)
-#Those are the nonTADbound-lincRNAs
+#Those are the nonTADbound-lincRNAs (there are 1090 of these)
+
+#==============================================================
+#Protein coding genes:
+
+overlap_pc <- read.table("pcgene_25overlap_TADb.bed")
+colnames(overlap_pc) <- c("chr", "start", "end", "gene", "strand")
+
+length(overlap_pc$gene)
+# pcgenes: 21544 overlap events were found using the same parameters.
+
+length(overlap_pc$gene[duplicated(overlap_pc$gene)])
+# pcgenes: 10207 are duplicates
+
+TADbound_pc <- overlap_pc[!duplicated(overlap_pc$gene),]
+#pcgenes: Out of the 14846 original genes, 11337 have at least 25% of their sequence overlapping the TAD boundaries.
+write.table(TADbound_pc, file = "TADbound-pcgene.bed", sep="\t", quote=F, col.names = F, row.names = F)
+# Those are the TADbound-pcgenes
+
+#Identifying nonTADbound-pcgenes
+nonTADbound_pc <- pc_ori[!(pc_ori$gene %in% TADbound_pc$gene),]
+write.table(nonTADbound_pc,file = "nonTADbound-pcgene.bed",sep="\t",quote = F,col.names = F,row.names = F)
+#Those are the nonTADbound-pcgenes (there are 3509 of these)
 
 #===================================================
-# Visualizing length of RNAs and TAD boundaries.
+# Visualizing length of lincRNAs, pcgenes and TAD boundaries.
 par(mfrow=c(1,3))
+hist(log10(TAD_boundaries$end - TAD_boundaries$start))
 hist(log10(TADbound_lincRNA$end - TADbound_lincRNA$start))
 hist(log10(RNA_ori$end - RNA_ori$start))
-hist(log10(TAD_boundaries$end - TAD_boundaries$start))
+hist(log10(TADbound_pc$end - TADbound_pc$start))
+hist(log10(pc_ori$end - pc_ori$start))
 #Summary statistics
+summary(TAD_boundaries$end - TAD_boundaries$start)
+#lincRNA:
 summary(TADbound_lincRNA$end - TADbound_lincRNA$start)
 summary(RNA_ori$end - RNA_ori$start)
-summary(TAD_boundaries$end - TAD_boundaries$start)
 summary(nonTADbound_lincRNA$end - nonTADbound_lincRNA$start)
+#pcgenes
+summary(TADbound_pc$end - TADbound_pc$start)
+summary(pc_ori$end - pc_ori$start)
+summary(nonTADbound_pc$end - nonTADbound_pc$start)
 
 
