63e098c2a2c7d4deee92511a5fd84ec30ee9abb9
cmdoret <cyril.mattheydoret@gmail.com>
2016-10-07 18:24:29 +0200

    added TODO list to remember things on monday

---
+++ TADboundaries_processing/TADb_cutter.R
@@ -0,0 +1,6 @@
+# This script intends to cut TAD boundaries so that a boundary does not overlap with the boundary of the next TAD.
+
+# Cyril Matthey-Doret 
+# 07.10.2016
+######################################
+
