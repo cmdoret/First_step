6a1796666dc6a558dc121f552e7c18fd4af69c1e
Cyril Matthey-Doret <cmatthe5@wl6s-156-86.unil.ch>
2016-10-10 15:09:30 +0200

    beginning to work on uni computer

--- TADboundaries_processing/TADb_cutter.R
+++ TADboundaries_processing/TADb_cutter.R
@@ -4,3 +4,20 @@
 # 07.10.2016
 ######################################
 
+setwd("/home/cyril/Documents/First_step/data/")
+
+TADb10 <- read.table("TAD/TAD_boundaries10.bed")
+TADb10 <- TADb10[,-4]
+colnames(TADb10)<-c("chr", "start","end","ID")
+
+#by(chr,apply(TADb,fun))
+
+over.bound <- function(TAD1,TAD2)
+  
+cut.bound<-function(df){
+  for(id in df$ID){
+    for(o in df$ID[-(df$ID==id),]){
+      
+    }
+  }
+}
