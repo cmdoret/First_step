e7100c4ae2781ebcdc28d3a12336be32d80c1f95
cmdoret <cyril.mattheydoret@gmail.com>
2016-10-13 21:41:29 +0200

    writing script to count transcripts per bin

--- TAD-lincRNA-overlap/bins_RNA_overlap.R
+++ TAD-lincRNA-overlap/bins_RNA_overlap.R
@@ -5,13 +5,21 @@
 # 13.10.2016
 ##################################
 
-# Loading data
-
-setwd("/Users/cmatthe5/Documents/First_step/data/")
+# Loading data:
+setwd("/home/cyril/Documents/First_step/data/")
+#setwd("/Users/cmatthe5/Documents/First_step/data/")
 TADbins <- read.table("TAD/merged/merged_TADbins.txt")
 lincRNA <- read.table("linc_RNA/LCL.expressed.lincRNA.bed")
 colnames(lincRNA) <- c("chr","start","end","ID","strand")
 colnames(TADbins) <- c("ID","chr","pos","start","end")
+over_pc <- read.table("../TAD-lincRNA-overlap/bin-pcgenes_overlap.bed")
+over_lincRNA <- read.table("../TAD-lincRNA-overlap/bin-lincRNA_overlap.bed")
 #================================
 
-# 
+# Counting overlaps:
+
+TADbins <- cbind(TADbins, lincRNA=rep(0,length(TADbins$ID)),pcgene=rep(0,length(TADbins$ID)))
+
+for(id in row(TADbins)){
+  print(id)
+}
