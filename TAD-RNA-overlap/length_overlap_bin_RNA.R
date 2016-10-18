b656c18bd94ebb3c36b049c0632ea93b60364fb4
cmdoret <cyril.mattheydoret@gmail.com>
2016-10-18 18:40:01 +0200

    wrote discussion of results in report 3

--- TAD-RNA-overlap/length_overlap_bin_RNA.R
+++ TAD-RNA-overlap/length_overlap_bin_RNA.R
@@ -5,51 +5,40 @@
 ###################################
 
 #Loading data:
-setwd("/home/cyril/Documents/Master/sem_1/First_step/data/")
-over.len_pc_bins<-read.table("TAD/merged/overlaps/length_bin-pcgenes_overlap5.bed")
-over.len_lincRNA_bins<-read.table("TAD/merged/overlaps/length_bin-lincRNA_overlap5.bed")
+#setwd("/home/cyril/Documents/Master/sem_1/First_step/data/")
+setwd("/home/cyril/Documents/First_step/data/")
+over.len_pc_bins<-read.table("TAD/merged/overlaps/length_bin-pcgenes_overlap10.bed")
+over.len_lincRNA_bins<-read.table("TAD/merged/overlaps/length_bin-lincRNA_overlap10.bed")
 colnames(over.len_pc_bins)=colnames(over.len_lincRNA_bins) <- c("bin.chr","bin.start","bin.end","bin.ID","bin.pos",
                                                                 "RNA.chr","RNA.start","RNA.end","RNA.ID","RNA.strand","overlap.length")
 #=================================
 
-
-
-# Should I include all overlaps in the statistics, or only those >0 ?
-# Let's try both
-
-# Without 0's:
-
-medover.lincRNA_wo <-by(data = over.len_lincRNA_bins$overlap.length[over.len_lincRNA_bins$overlap.length>0],
+# Normalizing per bin length
+over.len_pc_bins <- data.frame(over.len_pc_bins, norm.over=over.len_pc_bins$overlap.length/(over.len_pc_bins$RNA.end - over.len_pc_bins$RNA.start))
+over.len_lincRNA_bins <- data.frame(over.len_lincRNA_bins,norm.over=over.len_lincRNA_bins$overlap.length/(over.len_lincRNA_bins$RNA.end - over.len_lincRNA_bins$RNA.start))
+      
+medover.lincRNA <-by(data = over.len_lincRNA_bins$norm.over[over.len_lincRNA_bins$overlap.length>0],
    INDICES = over.len_lincRNA_bins$bin.pos[over.len_lincRNA_bins$overlap.length>0],
    FUN = median,simplify = F)
 
-medover.pcgene_wo <-by(data = over.len_pc_bins$overlap.length[over.len_pc_bins$overlap.length>0],
+medover.pcgene <-by(data = over.len_pc_bins$norm.over[over.len_pc_bins$overlap.length>0],
                         INDICES = over.len_pc_bins$bin.pos[over.len_pc_bins$overlap.length>0],
                         FUN = median,simplify = F)
 
-#with 0's:
-
-
-medover.lincRNA_w <-by(data = over.len_lincRNA_bins$overlap.length,
-                        INDICES = over.len_lincRNA_bins$bin.pos,
-                        FUN = median,simplify = F)
-
-medover.pcgene_w <-by(data = over.len_pc_bins$overlap.length,
-                       INDICES = over.len_pc_bins$bin.pos,
-                       FUN = median,simplify = F)
-
 #===============================
 # Visualize results:
-thr <- 1/length(medover.pcgene_w) + 0.6/length(medover.pcgene_w)
-medover_bin_RNA <- data.frame(pos=factor(names(unlist(medover.pcgene_w)),
+thr <- 1/length(medover.pcgene) + 0.6/length(medover.pcgene)
+medover_bin_RNA <- data.frame(pos=factor(names(unlist(medover.pcgene)),
                                        c(paste0("L",seq(from=(0.3/thr),to=1,by=-1)),
                                          seq(from=thr*100,to=100,by=thr*100),
                                          paste0("R",seq(from=1,to=(0.3/thr),by=1))),ordered = T),
-                            median.pc.w=unname(unlist(medover.pcgene_w)),
-                            median.pc.wo=unname(unlist(medover.pcgene_wo)),
-                            median.lincRNA.w=unname(unlist(medover.lincRNA_w)),
-                            median.lincRNA.wo=unname(unlist(medover.lincRNA_wo)))
+                            median.pc.over_med=unname(unlist(medover.pcgene)),
+                            median.lincRNA.over_med=unname(unlist(medover.lincRNA)))
 
-plot(medover_bin_RNA$pos,medover_bin_RNA$median.pc.wo)
-plot(medover_bin_RNA$pos,medover_bin_RNA$median.lincRNA.wo)
+par(mfrow=c(2,1))
+plot(medover_bin_RNA$pos,medover_bin_RNA$median.lincRNA.over_med)
+plot(medover_bin_RNA$pos,medover_bin_RNA$median.pc.over_med)
+
+write.table(medover_bin_RNA,file = "TAD/merged/median_overlap_bin10-RNA.txt",sep="\t",quote = F,col.names = F,row.names = F)
+
 
