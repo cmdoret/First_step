3f048b153c85e3f991dfa58e7c8850da32bab541
cmdoret <cyril.mattheydoret@gmail.com>
2016-11-16 23:10:13 +0100

    added alternative short TAD definition. Includes large TAD in cases overlap is not complete

--- TADboundaries_processing/small_TADs.R
+++ TADboundaries_processing/small_TADs.R
@@ -4,6 +4,7 @@
 
 # Loading data:
 setwd("/home/cyril/Documents/First_step/data/")
+setwd("/home/cyril/Documents/Master/sem_1/First_step/data/")
 setwd("/Users/cmatthe5/Documents/First_step/data/")
 TAD <- read.table("TAD/GM12878_TAD_domains.bed")
 TAD <- TAD[,c(1,2,3,5)]
@@ -60,4 +61,38 @@ large_b <- reduce_TAD(TADb) # Storing Large boundaries that need to be removed
 short_b <- TADb[!(TADb$ID %in% unname(unlist(large_b))),]  # Removing longer overlapping boundaries
 options(scipen=999) 
 write.table(short_b, file="TAD/short_in5_boundaries.bed",sep="\t",quote = F,col.names=F,row.names=F)
+
+##############################
+# DIFFERENT VERSIONS FOLLOWING DISCUSSION of 16.11.2016 
+#======================================
+# Different version only discarding TADs if a smaller one is entirely inside.
+inbig_TAD <- function(TAD){
+  blacklist <- list()
+  for (c in levels(TAD$chr)){  # Doing operations separately by chromosomes
+    blacklist[[c]] <- c()
+    TAD_tmp<-Intervals(matrix(c(TAD$start[TAD$chr==c],
+                                TAD$end[TAD$chr==c]),ncol=2))
+    rownames(TAD_tmp) <- TAD$ID[TAD$chr==c]  # Naming intervals after TADs
+    over <-interval_overlap(TAD_tmp,TAD_tmp)  # Calling overlaps between all TADs
+    for(k in names(over)){  # Iterating over TAD names
+      if(length(over[[k]])>1){  # If the number of overlaps for a TAD is gt 1 (i.e. overlaps not only with itself)
+        for(t in over[[k]]){  # Iterating over items overlapping this TAD
+          if(k!=names(over[t]) # Only counting overlaps that are not the TAD itself
+             & size(TAD_tmp[k])<=size(TAD_tmp[t])   # Removing TAD if larger or same size.
+             & size(interval_intersection(TAD_tmp[k],TAD_tmp[t]))>=size(TAD_tmp[k])){  # Removing TAD if it completely encompass smaller one
+            blacklist[[c]] <- append(blacklist[[c]],rownames(TAD_tmp[t])) # Contains only large TADs that need to be removed
+          }
+        }
+      }
+    }
+  }
+  return(blacklist)
+}
+large <-inbig_TAD(TAD)  # Storing the IDs of all large TADs
+
+options(scipen=999)  # Preventing R from using scientific notation (other programs such as bedtools and genome browsers do not like it)
+short <- TAD[!(TAD$ID %in% unname(unlist(large))),]  # Removing all large TADs from the original list
+write.table(short,"TAD/short_TADs.bed",sep="\t",quote = F,col.names=F,row.names=F)
+
+# Boundaries extending in and out:
 
