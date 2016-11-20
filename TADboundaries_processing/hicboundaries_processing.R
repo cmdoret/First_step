ec6a7cf639278bb55eccf9cedb00c0c0c1146ace
cmdoret <cyril.mattheydoret@gmail.com>
2016-11-20 02:21:36 +0100

    new version for report on expression/contact correlations is ready

--- TADboundaries_processing/hicboundaries_processing.R
+++ TADboundaries_processing/hicboundaries_processing.R
@@ -1,4 +1,5 @@
 setwd("/Users/cmatthe5/Documents/First_step/data/")
+setwd("/home/cyril/Documents/Master/sem_1/First_step/data/")
 hicboundfull<-read.table("TAD/hicboundaries/HICBOUND3.txt",header=T)
 library(dplyr)
 
@@ -7,4 +8,5 @@ merged<-bind_rows(hicboundfull[,c("chr","Lbound.start","Lbound.end")],hicboundfu
 merged$Lbound.start[is.na(merged$Lbound.start)] <- merged$Rbound.start[is.na(merged$Lbound.start)]
 merged$Lbound.end[is.na(merged$Lbound.end)] <- merged$Rbound.end[is.na(merged$Lbound.end)]
 merged <- merged[,c(1,2,3)]
+options(scipen=999)
 write.table(merged,"TAD/hicboundaries/ucsc_chr3.txt",col.names=F,row.names=F,sep="\t",quote=F)
